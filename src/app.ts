cd10b504a41a7c913296024ac7dd60753386e69c
Bryan <bryanbatista79688@gmail.com>
2023-02-05 15:01:32 -0300

    feat: database connection

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { application, Application } from "express";
+import { startDatabase } from "./database";
 
 const app: Application = express();
 const port = 3000;
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  await startDatabase();
   console.log(`App is running on port ${port}`);
 });
