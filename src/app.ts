5f3c7bba5fc991dcdf17beec623363ba6d709dd8
Bryan <bryanbatista79688@gmail.com>
2023-02-06 08:30:15 -0300

    feat: create and read routes for app.ts, sql table code, create validation

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express, { application, Application } from "express";
 import { startDatabase } from "./database";
+import { createMovie, readMovies } from "./functions";
 
 const app: Application = express();
 const port = 3000;
+app.use(express.json());
+
+app.post("/movies/create", createMovie);
+app.get("/movies/list", readMovies);
 
 app.listen(port, async () => {
   await startDatabase();
